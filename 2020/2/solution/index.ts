bbc1d7d4e5cce3e8a7444de3e6b27fb6a5629b97
Erkka Mutanen <erkka.mutanen@gmail.com>
2020-12-15 21:12:34 +0200

    Solution 2-2

--- 2020/2/solution/index.ts
+++ 2020/2/solution/index.ts
@@ -1,11 +1,11 @@
 import { PasswordPolicyFileReader } from './PasswordPolicyFileReader';
-import { Validator } from './Validator';
 import { PasswordPolicy } from './PasswordPolicy';
-import { validatePasswordPolicy } from './validatePasswordPolicy';
+import { Validator } from './Validator';
+import { validatePasswordPolicy, validatePositionalPasswordPolicy } from './validatePasswordPolicy';
 
 const passwordPolicyFileReader = new PasswordPolicyFileReader('../input/input.txt');
 passwordPolicyFileReader.read();
 
-const passwordValidator = new Validator<PasswordPolicy>(passwordPolicyFileReader.data, validatePasswordPolicy);
+const passwordValidator = new Validator<PasswordPolicy>(passwordPolicyFileReader.data, validatePositionalPasswordPolicy);
 passwordValidator.validate();
 console.log(`Final result: Password validator found ${passwordValidator.validItemsAmount} valid passwords.`);
--- 2020/2/solution/validatePasswordPolicy.ts
+++ 2020/2/solution/validatePasswordPolicy.ts
@@ -1,17 +1,34 @@
 import { PasswordPolicy } from "./PasswordPolicy";
 
-export const validatePasswordPolicy = (passwordPolicy: PasswordPolicy): Boolean => {
+export const validatePasswordPolicy = ({ policy, password }: PasswordPolicy): Boolean => {
   // business logic here
-  const policyLength = passwordPolicy.policy.length;
-  const targetChar = passwordPolicy.policy[policyLength - 1];
-  const occurrenceCount = passwordPolicy.password.split(targetChar).length - 1; // fast count with split
-  const minSplit = passwordPolicy.policy.split('-');
+  const policyLength = policy.length;
+  const targetChar = policy[policyLength - 1];
+  const occurrenceCount = password.split(targetChar).length - 1; // fast count with split
+  const minSplit = policy.split('-');
   const maxSplit = minSplit[1].split(' ');
 
   const minCount = parseInt(minSplit[0]);
   const maxCount = parseInt(maxSplit[0]);
 
   if(occurrenceCount >= minCount && occurrenceCount <= maxCount) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+export const validatePositionalPasswordPolicy = ({ policy, password }: PasswordPolicy): Boolean => {
+  const policyLength = policy.length;
+  const targetChar = policy[policyLength - 1];
+  // const occurrenceCount = passwordPolicy.password.split(targetChar).length - 1;
+  const occurrence1Split = policy.split('-');
+  const occurrence2Split = occurrence1Split[1].split(' ');
+
+  const occurrence_one = password[parseInt(occurrence1Split[0]) - 1] === targetChar; // 1-based indexing in passwords for position
+  const occurrence_two = password[parseInt(occurrence2Split[0]) - 1] === targetChar;
+
+  if(occurrence_one != occurrence_two) {
     return true;
   } else {
     return false;
