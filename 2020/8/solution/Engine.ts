997b027d13544b267c2385672843286447c79864
Erkka Mutanen <erkka.mutanen@gmail.com>
2021-01-22 17:18:30 +0200

    Solution 8-2

--- 2020/8/solution/Engine.ts
+++ 2020/8/solution/Engine.ts
@@ -2,19 +2,56 @@ import { HandheldCommand } from './HandheldHaltingDataMapping';
 
 export class Engine {
   private cmdsExecuted: number[];
+  private cmdsSwapped: Boolean[];
+  private loopSwapped: Boolean = false;
 
   constructor(public operationList: HandheldCommand[], private accumulator: number) {
-    this.cmdsExecuted = Array(operationList.length).fill(0);
+    this.cmdsExecuted = this.createValArray(operationList.length, 0);
+    this.cmdsSwapped = this.createValArray(operationList.length, false);
   }
 
-  run(): number {
+  createValArray(len: number, val: any) {
+    return Array(len).fill(val);
+  }
+
+  swapNopJump(op: HandheldCommand): HandheldCommand {
+    if(op.command === 'nop') return { command: 'jmp', value: op.value };
+    if(op.command === 'jmp') return { command: 'nop', value: op.value };
+    return op;
+  }
+
+  acc(accumulate: number) {
+    this.accumulator+=accumulate;
+    // console.log(`Accumulate: accumulator now ${this.accumulator}`);
+  }
+
+  run(swap: Boolean): number {
+    // Setup for each loop through program
+    this.cmdsExecuted = this.createValArray(this.operationList.length, 0);
+    this.accumulator = 0;
+    this.loopSwapped = false;
+
     for(let i = 0; i < this.operationList.length; i++) {
-      const op = this.operationList[i];
-      if(this.cmdsExecuted[i] > 0) {
-        console.log(`We are in infinite loop! Accumulator value before the operation is `, this.accumulator);
-        return this.accumulator;
-      }
-      const cmd = op.command;
+      let op = this.operationList[i];
+        if(this.cmdsExecuted[i] > 0) {
+          if(swap) {
+            console.log(`We swapped command on line ${i}`);
+            return -1;
+          }
+          console.log(`We are in infinite loop! Accumulator value before the operation is `, this.accumulator);
+          return -1;
+        }
+
+        if(swap) {
+          if(!this.cmdsSwapped[i] && !this.loopSwapped){
+            op = this.swapNopJump(op);
+            this.cmdsSwapped[i] = true;
+            this.loopSwapped = true;
+  
+          }
+        }
+
+        const cmd = op.command;
         switch(cmd) {
           case 'acc': 
             this.acc(op.value);
@@ -33,14 +70,10 @@ export class Engine {
             }
             break;
           default: 
-            console.error('program error!');
+            console.error('program error! unknown command input');
         }
 
     }
-  }
-
-  acc(accumulate: number) {
-    this.accumulator+=accumulate;
-    // console.log(`Accumulate: accumulator now ${this.accumulator}`);
+    return this.accumulator;
   }
 }
--- 2020/8/solution/index.ts
+++ 2020/8/solution/index.ts
@@ -4,7 +4,16 @@ import { Engine } from './Engine';
 
 const handheldDataFileReader = new FileReader<HandheldCommand>('../input/handheldHaltingInput.txt', dataMapMethod);
 handheldDataFileReader.read();
-console.dir(handheldDataFileReader.items, { depth: null });
 
+// Part 1
 const engine = new Engine(handheldDataFileReader.items, 0);
-engine.run();
+engine.run(false);
+
+// Part 2 
+// runs the engine until we find the right solution
+let finalAccVal = -1; // initial value
+while(finalAccVal < 0) { // returns -1 for infinite loop
+  finalAccVal = engine.run(true);
+}
+
+console.log(`Final accumulator value when program terminates correctly is ${finalAccVal}!`);
